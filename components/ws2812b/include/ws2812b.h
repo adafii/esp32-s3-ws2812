c8ab2a13ad0aaba0e66b85f78ac538ed639ff726
Liia Rimpiläinen <liia@oru.fi>
2024-01-18 22:40:15 +0200

    Static led color

--- components/ws2812b/include/ws2812b.h
+++ components/ws2812b/include/ws2812b.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#define GPIO_NUM            46
-#define RESOLUTION_HZ       (10 * 1000 * 1000)        // 10 MHz, 1 tick = 0.1 µs
-#define MEM_BLOCK_SYMBOLS   64
-#define TX_QUEUE_DEPTH      4
-#define INTR_PRIORITY       0
+#define GPIO_NUM 46
+#define RESOLUTION_HZ (10 * 1000 * 1000)  // 10 MHz, 1 tick = 0.1 µs
+#define MEM_BLOCK_SYMBOLS 48
+#define TX_QUEUE_DEPTH 4
+#define INTR_PRIORITY 0
+
+#define BIT0H 4
+#define BIT0L 8
+#define BIT1H 8
+#define BIT1L 4
 
 void on(void);
--- components/ws2812b/ws2812b.c
+++ components/ws2812b/ws2812b.c
@@ -2,8 +2,8 @@
 #include "driver/gpio.h"
 #include "driver/rmt_tx.h"
 
-rmt_channel_handle_t install_tx_channel(void) {
-    rmt_tx_channel_config_t tx_channel_config = {
+void get_tx_channel(rmt_channel_handle_t* tx_channel) {
+    static rmt_tx_channel_config_t tx_channel_config = {
         .gpio_num = GPIO_NUM,
         .clk_src = RMT_CLK_SRC_DEFAULT,
         .resolution_hz = RESOLUTION_HZ,
@@ -16,10 +16,37 @@ rmt_channel_handle_t install_tx_channel(void) {
         .flags.with_dma = false,
     };
 
-    rmt_channel_handle_t tx_channel = NULL;
-    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_channel_config, &tx_channel));
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_channel_config, tx_channel));
+}
 
-    return tx_channel;
+void get_encoder(rmt_encoder_handle_t* encoder) {
+    static rmt_bytes_encoder_config_t bytes_encoder_config = {
+        .bit0.duration0 = BIT0H,
+        .bit0.level0 = 1,
+        .bit0.duration1 = BIT0L,
+        .bit0.level1 = 0,
+
+        .bit1.duration0 = BIT1H,
+        .bit1.level0 = 1,
+        .bit1.duration1 = BIT1L,
+        .bit1.level1 = 0,
+
+        .flags.msb_first = true,
+    };
+
+    ESP_ERROR_CHECK(rmt_new_bytes_encoder(&bytes_encoder_config, encoder));
+}
+
+void transmit(rmt_channel_handle_t* tx_channel, rmt_encoder_handle_t* encoder) {
+    static rmt_transmit_config_t transmit_config = {
+        .loop_count = 0,
+        .flags.eot_level = 0,
+    };
+
+    uint8_t payload[3] = {0, 255, 0};
+
+    ESP_ERROR_CHECK(rmt_enable(*tx_channel));
+    ESP_ERROR_CHECK(rmt_transmit(*tx_channel, *encoder, payload, 3, &transmit_config));
 }
 
 void on(void) {
@@ -29,4 +56,12 @@ void on(void) {
 
     gpio_reset_pin(46);
     gpio_set_direction(46, GPIO_MODE_OUTPUT);
+
+    rmt_channel_handle_t tx_channel = NULL;
+    rmt_encoder_handle_t encoder = NULL;
+
+    get_tx_channel(&tx_channel);
+    get_encoder(&encoder);
+
+    transmit(&tx_channel, &encoder);
 }
